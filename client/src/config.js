49e3f132b91e13272ee3774b1056da0a7a8673b8
Magda <>
2022-11-27 15:24:24 -0500

    bug fix

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosBaseUrl = axios.create({
-    baseUrl:"https://github-search-app-full-1.herokuapp.com"
+    baseUrl:"https://git.heroku.com/github-search-app-full-1.git"
 })
