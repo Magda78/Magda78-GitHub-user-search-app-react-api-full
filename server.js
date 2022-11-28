0494c5ccfa9e11e18cdcd61a5de4d938be0d0c8b
Magda <>
2022-11-27 23:33:07 -0500

    bug fix

--- server.js
+++ server.js
@@ -31,6 +31,17 @@ app.use(cors({ origin: 'https://api.github.com/users', credentials: true }));
 //};
 //app.use(cors(corsOptions));
 
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+	);
+	next();
+});
+
 app.get('/:user', (req, res) => {
 	const { user } = req.params;
 
